0ffd9338c6b6e483ed2c7b973ec41f9835537cc7
Oliver Doepner <oliver@doepner.net>
2012-12-07 20:10:55 -0400

    Use import

--- src/main/java/net/doepner/ui/action/IdAction.java
+++ src/main/java/net/doepner/ui/action/IdAction.java
@@ -1,12 +1,14 @@
 package net.doepner.ui.action;
 
 
+import javax.swing.Action;
+
 import net.doepner.IdentiedBy;
 
 /**
  * IdAction that can be more precisely identified by an action id
  */
-public interface IdAction extends javax.swing.Action, IdentiedBy<ActionId> {
+public interface IdAction extends Action, IdentiedBy<ActionId> {
 
     // no additional methods
 }
