549722535faf815d228df53932b3f9555b01f17e
odoepner <odoepner@gmail.com>
2015-02-15 13:49:44 -0400

    Do not log ftp credentials

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -1,8 +1,8 @@
-#! /bin/bash -x
+#! /bin/bash
 
-for file in $TRAVIS_BUILD_DIR/typepad-dist/target/*.zip
-do
-  curl -T "$file" \
-       -u $FTP_USER:$FTP_PASSWORD \
-       'ftp://doepner.net/~/public_html/dev/dist/ci-builds/typepad.zip'
+ftp_file_url='ftp://doepner.net/~/public_html/dev/dist/ci-builds/typepad.zip'
+
+for file in $TRAVIS_BUILD_DIR/typepad-dist/target/*.zip ; do
+  echo "Uploading $file to $ftp_file_url"
+  curl -T "$file" -u $FTP_USER:$FTP_PASSWORD "$ftp_file_url"
 done
