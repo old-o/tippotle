cf10de7271b187193824aa8b89b1a4d749a26f51
odoepner <odoepner@gmail.com>
2015-02-15 08:33:23 -0400

    Use travis build dir correctly

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -x
 
-for file in $TRAVIS_BUILD_DIR/typepad/typepad-dist/target/*.zip
+for file in $TRAVIS_BUILD_DIR/typepad-dist/target/*.zip
 do
   curl -T "$file" \
        -u $FTP_USER:$FTP_PASSWORD \
