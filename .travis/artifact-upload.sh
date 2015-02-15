4140a6eb260a44782bc4b13f2edc6e911df2e4b8
odoepner <odoepner@gmail.com>
2015-02-15 13:40:17 -0400

    Use permanent artifact name

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -4,5 +4,5 @@ for file in $TRAVIS_BUILD_DIR/typepad-dist/target/*.zip
 do
   curl -T "$file" \
        -u $FTP_USER:$FTP_PASSWORD \
-       'ftp://doepner.net/~/public_html/dev/dist/ci-builds/'
+       'ftp://doepner.net/~/public_html/dev/dist/ci-builds/typepad.zip'
 done
