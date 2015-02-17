165e95b9b492d10028c40ee5bead402799a0000c
odoepner <odoepner@gmail.com>
2015-02-17 18:04:04 -0400

    better image scaling and centering

--- typepad-impl/src/main/java/net/doepner/ui/images/ImagePanel.java
+++ typepad-impl/src/main/java/net/doepner/ui/images/ImagePanel.java
@@ -4,9 +4,16 @@ import net.doepner.ui.ImageContainer;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
-public class ImagePanel extends JPanel implements ImageContainer {
+import static java.awt.RenderingHints.KEY_ALPHA_INTERPOLATION;
+import static java.awt.RenderingHints.KEY_INTERPOLATION;
+import static java.awt.RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY;
+import static java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR;
+import static java.lang.Math.max;
+
+public final class ImagePanel extends JPanel implements ImageContainer {
 
     private Image image;
 
@@ -20,12 +27,31 @@ public class ImagePanel extends JPanel implements ImageContainer {
 
     @Override
     protected void paintComponent(Graphics g) {
+        final Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(KEY_INTERPOLATION, VALUE_INTERPOLATION_BILINEAR);
+        g2.setRenderingHint(KEY_ALPHA_INTERPOLATION, VALUE_ALPHA_INTERPOLATION_QUALITY);
+
         super.paintComponent(g);
 
         if (image != null) {
-            final Image img = image.getScaledInstance(
-                    getWidth(), getHeight(), Image.SCALE_SMOOTH);
-            g.drawImage(img, 0, 0, null);
+            final int imageWidth = image.getWidth(null);
+            final int imageHeight = image.getHeight(null);
+
+            final int width = getWidth();
+            final int heigth = getHeight();
+
+            final float xScale = imageWidth > width ? (float) imageWidth / (float) width : 1.0f;
+            final float yScale = imageHeight > heigth ? (float) imageHeight / (float) heigth : 1.0f;
+
+            final float scale = max(xScale, yScale);
+
+            final int targetWidth = (int) (imageWidth / scale);
+            final int targetHeight = (int) (imageHeight / scale);
+
+            final int widthMargin = (width - targetWidth) / 2;
+            final int heightMargin = (heigth - targetHeight) / 2;
+
+            g.drawImage(image, widthMargin, heightMargin, targetWidth, targetHeight, null);
         }
     }
 }
