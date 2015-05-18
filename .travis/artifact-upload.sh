84c42a9299025571896bd26145b6fd442ef506f3
odoepner <odoepner@gmail.com>
2015-05-18 09:43:39 -0300

    Error check for FTP upload script

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -3,5 +3,10 @@
 local_file="$(ls $TRAVIS_BUILD_DIR/typepad-dist/target/*.zip | head -n 1)"
 target_url='ftp://doepner.net/~/public_html/dev/dist/ci-builds/typepad.zip'
 
-echo "Uploading $local_file to $target_url"
-curl -u $FTP_USER:$FTP_PASSWORD -T "$local_file" "$target_url"
+if [ -z "$local_file" ]; then
+  echo "ERROR: Cannot upload. local_file is empty"
+  exit 1
+else
+  echo "Uploading $local_file to $target_url"
+  curl -u $FTP_USER:$FTP_PASSWORD -T "$local_file" "$target_url"
+fi
