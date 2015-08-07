6771473e885ec3b385a5d06225ab0b432e22cfd1
odoepner <oliver@doepner.net>
2015-08-07 16:22:06 -0300

    Fix Google Translate URL (was giving 502 errors)

--- impl/src/main/java/net/doepner/resources/GoogleTranslateUrls.java
+++ impl/src/main/java/net/doepner/resources/GoogleTranslateUrls.java
@@ -17,12 +17,15 @@ import static net.doepner.file.FileTypeEnum.mp3;
  */
 public final class GoogleTranslateUrls implements UrlProvider {
 
-    private static final String USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:21.0) " +
-            "Gecko/20100101 Firefox/21.0 Iceweasel/21.0";
+    private static final String USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:21.0) "
+            + "Gecko/20100101 Firefox/21.0 Iceweasel/21.0";
+
+    private static final String BASE_URL = "http://translate.google.com/translate_tts?"
+            + "ie=UTF-8&total=1&idx=0&client=t";
 
     @Override
     public URL url(String s, Language language) throws MalformedURLException {
-        return new URL("http://translate.google.com/translate_tts?ie=UTF-8"
+        return new URL(BASE_URL
                 + (language != null ? "&tl=" + urlEncode(language.code()) : "")
                 + "&q=" + urlEncode(s.toLowerCase()));
     }
