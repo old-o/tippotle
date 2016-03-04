3c1d3e9cb4dce0c344d398214a00b93874b0df58
odoepner <odoepner@gmail.com>
2016-03-04 19:21:21 -0400

    Let travis-ci deploy to bintray

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 local_file="$(ls $TRAVIS_BUILD_DIR/dist/target/*.zip | head -n 1)"
-target_url='ftp://doepner.net/~/public_html/dev/dist/ci-builds/tippotle.zip'
+target_url='https://api.bintray.com/content/odoepner/generic/tippotle/0.0.1/tippotle.zip?override=1&publish=1'
 
 if [ -z "$local_file" ]; then
   echo "ERROR: Cannot upload. local_file is empty"
   exit 1
 else
   echo "Uploading $local_file to $target_url"
-  curl -u $FTP_USER:$FTP_PASSWORD -T "$local_file" "$target_url"
+  curl -u$BINTRAY_USER:$BINTRAY_API_KEY -T "$local_file" "$target_url"
 fi
