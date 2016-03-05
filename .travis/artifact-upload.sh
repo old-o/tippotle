0f4837cfbf5dca3bdec2fa8b918a467167ed0ced
odoepner <odoepner@gmail.com>
2016-03-04 20:43:35 -0400

    Better error message

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-local_file="$(ls $TRAVIS_BUILD_DIR/dist/target/*.zip | head -n 1)"
-target_url='https://api.bintray.com/content/odoepner/generic/tippotle/0.0.1/tippotle.zip?override=1&publish=1'
+build_dir="$TRAVIS_BUILD_DIR/dist/target"
+zip_file="$(ls $build_dir/*.zip | head -n 1)"
+
+upload_url='https://api.bintray.com/content/odoepner/generic/tippotle/0.0.1/tippotle.zip?override=1&publish=1'
 
 if [ -z "$local_file" ]; then
-  echo "ERROR: Cannot upload. local_file is empty"
+  echo "ERROR: Cannot upload. No zip file found in $build_dir"
   exit 1
 else
-  echo "Uploading $local_file to $target_url"
-  curl -u$BINTRAY_USER:$BINTRAY_API_KEY -T "$local_file" "$target_url"
+  echo "Uploading $local_file to $upload_url"
+  curl -u$BINTRAY_USER:$BINTRAY_API_KEY -T "$local_file" "$upload_url"
 fi
