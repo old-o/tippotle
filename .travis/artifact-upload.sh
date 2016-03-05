706dbf6e74102d30182f1baf3fdb96fe0b2c8407
Oliver Doepner <oliver@doepner.net>
2016-03-05 15:10:09 -0400

    Fix bugs in deploy script

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -5,10 +5,10 @@ zip_file="$(ls $build_dir/*.zip | head -n 1)"
 
 upload_url='https://api.bintray.com/content/odoepner/generic/tippotle/0.0.1/tippotle.zip?override=1&publish=1'
 
-if [ -z "$local_file" ]; then
+if [ -z "$zip_file" ]; then
   echo "ERROR: Cannot upload. No zip file found in $build_dir"
   exit 1
 else
-  echo "Uploading $local_file to $upload_url"
-  curl -u$BINTRAY_USER:$BINTRAY_API_KEY -T "$local_file" "$upload_url"
+  echo "Uploading $zip_file to $upload_url"
+  curl -u$BINTRAY_USER:$BINTRAY_API_KEY -T "$zip_file" "$upload_url"
 fi
